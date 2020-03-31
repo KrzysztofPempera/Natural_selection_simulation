d613bce0e3659e8661fd37f779d3c024d1072580
Krzysztof Pempera <krzpem@st.amu.edu.pl>
2020-03-31 23:01:54 +0200

    Added basic display

---
+++ Natural_selection_simulation/carrot.py
@@ -0,0 +1,31 @@
+import random as rnd
+import pygame as pg
+from colors import *
+
+
+# block size
+BLOCK_SIZE = 20
+
+# carrot class definition
+class carrot(object):
+    
+    #constructor
+    def __init__(self, surface, minX, maxX, minY, maxY):
+        self.surface = surface
+        self.posX = rnd.randint(minX, maxX - 1)
+        self.posY = rnd.randint(minY, maxY - 1)
+
+        self.colour = RED
+
+    def getPosition(self):
+        return (self.posX, self.posY)
+
+    def move(self):
+        self.posX += 1
+        self.posY += 1
+
+    def draw(self):
+        sur = self.surface
+        cPosition = self.getPosition()
+
+        pg.draw.rect(sur,self.colour,(cPosition[0],cPosition[1],BLOCK_SIZE,BLOCK_SIZE))
---
+++ Natural_selection_simulation/main.py
@@ -0,0 +1,69 @@
+import pygame as pg
+import carrot as crt
+from colors import *
+
+# screen size and game speed
+WIDTH      = 400
+HEIGHT     = 400
+SPEED      = 8
+SPEED_TICK = 2
+SPEED_INC  = 5
+SHORT      = 12
+LONG       = 1
+ROWS       = 20
+
+pg.init()
+
+# Set up the drawing window
+clock = pg.time.Clock()
+screen = pg.display.set_mode([WIDTH, HEIGHT])
+pg.display.set_caption('Simulation')
+
+# set up food and rabbit
+food = crt.carrot(screen, 1, WIDTH - 20, 1, HEIGHT - 20)
+
+# draw grid 
+#def drawGrid(surface, rows, width):
+#    gridSize = 20
+
+#    x = 0
+#    y = 0
+
+#    for i in range(rows):
+#        x = x + gridSize
+#        y = y + gridSize
+
+#        pg.draw.line(surface, (WHITE), (x,0),(x,width))
+#        pg.draw.line(surface, (WHITE), (0,y),(width,y))
+
+#def drawScreen(surface, rows, width):
+
+#    surface.fill(BLACK)
+#    drawGrid(surface, rows, width)
+#    pg.display.flip()  
+#    pg.display.update()
+screen.fill(GREEN_DARK)
+pg.display.flip()
+
+running = True
+while running:
+    
+    #game speed
+    clock.tick(SPEED)
+
+    #draw food, rabbit, screen
+    #drawScreen(screen, ROWS, WIDTH)
+    food.draw()
+
+    pg.draw.rect(screen,RED,(1,1,20,20))
+    pg.display.update()
+    screen.fill(GREEN_DARK)
+    food.move()
+    # quit
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False 
+            pg.quit()
+
+    print("loop")
+
